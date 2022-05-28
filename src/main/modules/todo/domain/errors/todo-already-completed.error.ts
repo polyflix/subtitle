2210e6d1cc55e19ae39d2f1902f6719d44c6dd8f
alexandre.burgoni <alexandre.burgoni@etu.umontpellier.fr>
2022-05-28 09:47:42 +0200

    chore(boilerplate): remove todo

--- src/main/modules/todo/domain/errors/todo-already-completed.error.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from "../entities/todo.entity";
-
-export class TodoAlreadyCompletedError extends Error {
-    constructor(todo: Todo) {
-        super(`${todo.getTitle()} already completed.`);
-    }
-}
